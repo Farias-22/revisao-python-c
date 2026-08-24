ef4c0d670c54627491393f9c368d13c4109db984
Farias-22 <felipe.farias.220708@gmail.com>
2026-08-24 00:10:34 -0300

    Resolução questão 5

---
+++ questao05.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int a, b;
+    
+    printf("Primeiro valor: ");
+    scanf("%d", &a);
+
+    printf("Segundo valor: ");
+    scanf("%d", &b);
+
+    if (a > b){
+        printf("Maior: %d", a);
+    } else {
+        printf("Maior: %d", b);
+    }
+    return 0;
+}
